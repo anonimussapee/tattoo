19dafb84d42a4ba8bbb9dd94296e88308c252b6d
anonimussapee <librededrogas420@gmail.com>
2022-12-25 17:56:23 -0500

    realize el nav fixed y agrege el boton wpp y añadi contenido de introduccion

--- src/assets/scroll.js
+++ src/assets/scroll.js
@@ -1,4 +1,6 @@
 window.addEventListener('scroll', function(){
-    var header=document.querySelector("header");
-    header.classList.toggle("header_fixed",this.window.scrollY>0);
-})
+    var header=document.querySelector(".header-nav");
+    var space=document.querySelector(".ask_cover");
+    header.classList.toggle("nav_fixed",this.window.scrollY>0);
+    space.classList.toggle("ask_cover_space",this.window.scrollY>0);
+});
