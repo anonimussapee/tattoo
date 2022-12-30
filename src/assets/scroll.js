d9cca1196722895e372382adbe7b2d09a3b2413c
anonimussapee <librededrogas420@gmail.com>
2022-12-30 12:23:11 -0500

    agregado el footer y el script para que funcione en menuburger

--- src/assets/scroll.js
+++ src/assets/scroll.js
@@ -4,3 +4,15 @@ window.addEventListener('scroll', function(){
     header.classList.toggle("nav_fixed",this.window.scrollY>0);
     space.classList.toggle("ask_cover_space",this.window.scrollY>0);
 });
+var burger_menu=document.querySelector(".menu--burger-slider");
+var burger=document.querySelector(".menu--burger");
+var burger_close=document.querySelector(".burger-close");
+var chatwp=document.querySelector(".chat_wpp");
+burger.addEventListener("click",function(){
+burger_menu.classList.add("open");
+chatwp.classList.add("hidden");
+});
+burger_close.addEventListener("click", function(){
+burger_menu.classList.remove("open");
+chatwp.classList.remove("hidden");
+});
